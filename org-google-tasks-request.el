e8a2b501d54614f23dfcc8a09f6162c824373920
Yoshihide Chubachi <yoshi@chubachi.net>
2022-03-20 17:54:52 +0900

    Create id table

--- org-google-tasks-request.el
+++ org-google-tasks-request.el
@@ -4,6 +4,7 @@
 
 (require 'ht)
 (require 'oauth2)
+(require 'el-mock)
 
 (defcustom my/gtasks-client-secret-json ""
   "JSON file location to client secret."
@@ -59,7 +60,6 @@ REQUEST-DATA is any emacs lisp object that json-serialize understands."
 			     (encode-coding-string (json-serialize ,request-data) 'utf-8))))
 	  (response (my/parse-http-response response-buffer))
 	  (body (decode-coding-string (plist-get response :body) 'utf-8)))
-     (message body)
      (json-parse-string body)))
 
 ;;; Google Tasks APIs for tasklists.
@@ -88,3 +88,46 @@ Usage:
 ;; (my/api-tasklists-insert '(:title "はひふへほ"))
 ;; (my/api-tasklists-insert '((title . "はひふ")))
 ;; (my/api "https://tasks.googleapis.com/tasks/v1/users/@me/lists" "POST" '(:title "さしすせそ"))
+
+(defun my/api-tasks-list (tasklist)
+  "Returns all tasks in the specified task list.
+
+See URL https://developers.google.com/tasks/reference/rest/v1/tasks/list"
+  (my/api (format
+	   "https://tasks.googleapis.com/tasks/v1/lists/%s/tasks" tasklist)
+	  "GET"))
+
+;;; Utils
+(defun my/create-id-table ()
+  "Create a hash table for looking up tasklist or task items by the id."
+  (let* ((table (ht-create))
+	 (tasklists (my/api-tasklists-list))
+	 (tasklists-items (ht-get tasklists "items")))
+    (dolist (tasklist (cl-coerce tasklists-items 'list))
+      (let* ((tasklist-id (ht-get tasklist "id")))
+		;; Set the tasklist to the table.
+		(ht-set! table tasklist-id tasklist)
+		;; Get the tasks under the tasklist.
+		(let* ((tasks (my/api-tasks-list tasklist-id))
+		       (tasks-items (ht-get tasks "items")))
+		  (message "tasks:\n%s\nend:" tasks)
+		  (dolist (task (cl-coerce tasks-items 'list))
+		    (let ((task-id (ht-get task "id")))
+		       ;; Set the task to the table.
+		       (ht-set! table task-id task))))))
+    table))
+
+(ert-deftest my/create-id-table-test ()
+  (with-mock
+    (stub my/api-tasklists-list =>
+	  #s(hash-table test equal data
+                        ("kind" "tasks#taskLists" "etag" "\"MjA0MzIwOTcyNw\"" "items" [#s(hash-table test equal data ("kind" "tasks#taskList" "id" "MDc1MzA1NTQ1OTYxODU5MTEwMTg6MDow" "etag" "\"LTE1Nzc5OTMyNDA\"" "title" "Tasklist" "updated" "2022-03-19T11:30:48.118Z" "selfLink" "https://www.googleapis.com/tasks/v1/users/@me/lists/MDc1MzA1NTQ1OTYxODU5MTEwMTg6MDow")) #s(hash-table test equal data ("kind" "tasks#taskList" "id" "MkhZNnRBVGE4eFk2UW1sdw" "etag" "\"LTE4NTg3MTY1ODQ\"" "title" "タスク" "updated" "2022-03-16T05:32:05.318Z" "selfLink" "https://www.googleapis.com/tasks/v1/users/@me/lists/MkhZNnRBVGE4eFk2UW1sdw")) #s(hash-table test equal data ("kind" "tasks#taskList" "id" "M21GQnJNQm84YXdCWVZRcw" "etag" "\"LTE1NzA3ODUwNzE\"" "title" "はひふへほ" "updated" "2022-03-19T13:30:56.845Z" "selfLink" "https://www.googleapis.com/tasks/v1/users/@me/lists/M21GQnJNQm84YXdCWVZRcw"))])))
+    (stub my/api-tasks-list =>
+	  #s(hash-table test equal data
+			("kind" "tasks#tasks" "etag" "\"LTE1Nzc5OTMyNDA\"" "items" [#s(hash-table size 9 test equal rehash-size 1.5 rehash-threshold 0.8125 data ("kind" "tasks#task" "id" "YWlqV0hsRV9lYVlQdkx5MQ" "etag" "\"LTIxMDQ5NTgyODI\"" "title" "Task2 with note" "updated" "2022-03-13T09:08:03.000Z" "selfLink" "https://www.googleapis.com/tasks/v1/lists/MDc1MzA1NTQ1OTYxODU5MTEwMTg6MDow/tasks/YWlqV0hsRV9lYVlQdkx5MQ" "position" "00000000000000000001" "notes" "This is note string.
+Hello World!" "status" "needsAction")) #s(hash-table test equal data ("kind" "tasks#task" "id" "dlAzdXRlWDh2Z0dsck4xcQ" "etag" "\"MjA3MjIxMDY4Nw\"" "title" "Task 7 as sub-task" "updated" "2022-03-12T00:24:45.000Z" "selfLink" "https://www.googleapis.com/tasks/v1/lists/MDc1MzA1NTQ1OTYxODU5MTEwMTg6MDow/tasks/dlAzdXRlWDh2Z0dsck4xcQ" "parent" "aEQ1TjhNOGRiS3p6VmR4dw" "position" "00000000000000000000" "status" "needsAction")) #s(hash-table test equal data ("kind" "tasks#task" "id" "aEQ1TjhNOGRiS3p6VmR4dw" "etag" "\"MjA3MjE3NDc0OA\"" "title" "Task6 as parent" "updated" "2022-03-12T00:24:09.000Z" "selfLink" "https://www.googleapis.com/tasks/v1/lists/MDc1MzA1NTQ1OTYxODU5MTEwMTg6MDow/tasks/aEQ1TjhNOGRiS3p6VmR4dw" "position" "00000000000000000005" "status" "needsAction")) #s(hash-table test equal data ("kind" "tasks#task" "id" "Y1hxLXB0ZHJZb0x0Z3I0Mw" "etag" "\"MjA3MjE1ODc0MQ\"" "title" "Task5 with date time" "updated" "2022-03-12T00:23:53.000Z" "selfLink" "https://www.googleapis.com/tasks/v1/lists/MDc1MzA1NTQ1OTYxODU5MTEwMTg6MDow/tasks/Y1hxLXB0ZHJZb0x0Z3I0Mw" "position" "00000000000000000004" "status" "needsAction" "due" "2022-03-15T00:00:00.000Z")) #s(hash-table test equal data ("kind" "tasks#task" "id" "aUhGRlpRMXhqbmZrN1JsQQ" "etag" "\"MjA3MjE0NDc4NQ\"" "title" "Task4 with date (repeat)" "updated" "2022-03-12T00:23:38.000Z" "selfLink" "https://www.googleapis.com/tasks/v1/lists/MDc1MzA1NTQ1OTYxODU5MTEwMTg6MDow/tasks/aUhGRlpRMXhqbmZrN1JsQQ" "position" "00000000000000000003" "status" "needsAction" "due" "2022-03-15T00:00:00.000Z")) #s(hash-table test equal data ("kind" "tasks#task" "id" "TDlONFk0TThJT1VGb0h6RQ" "etag" "\"MjA3MjA4NTAzMA\"" "title" "Tasks3 with date" "updated" "2022-03-12T00:22:39.000Z" "selfLink" "https://www.googleapis.com/tasks/v1/lists/MDc1MzA1NTQ1OTYxODU5MTEwMTg6MDow/tasks/TDlONFk0TThJT1VGb0h6RQ" "position" "00000000000000000002" "status" "needsAction" "due" "2022-03-31T00:00:00.000Z")) #s(hash-table test equal data ("kind" "tasks#task" "id" "cF9ORW8yYWgyNTVES1dIbg" "etag" "\"MjA3MjA0NDc2MQ\"" "title" "Task1" "updated" "2022-03-12T00:21:59.000Z" "selfLink" "https://www.googleapis.com/tasks/v1/lists/MDc1MzA1NTQ1OTYxODU5MTEwMTg6MDow/tasks/cF9ORW8yYWgyNTVES1dIbg" "position" "00000000000000000000" "status" "needsAction"))])))
+    (let ((result))
+      (setq result (ht-keys (my/create-id-table)))
+      (should (equal
+	       (format "%s" result)
+	       "(M21GQnJNQm84YXdCWVZRcw MkhZNnRBVGE4eFk2UW1sdw cF9ORW8yYWgyNTVES1dIbg TDlONFk0TThJT1VGb0h6RQ aUhGRlpRMXhqbmZrN1JsQQ Y1hxLXB0ZHJZb0x0Z3I0Mw aEQ1TjhNOGRiS3p6VmR4dw dlAzdXRlWDh2Z0dsck4xcQ YWlqV0hsRV9lYVlQdkx5MQ MDc1MzA1NTQ1OTYxODU5MTEwMTg6MDow)")))))

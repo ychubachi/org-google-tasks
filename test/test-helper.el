7d455a8bfa1e8ae613d2e070b55639172cd8512e
Yoshihide Chubachi <yoshi@chubachi.net>
2022-03-30 23:03:12 +0900

    Change test-helper.el

--- test/test-helper.el
+++ test/test-helper.el
@@ -2,13 +2,13 @@
 (message "Running tests on Emacs %s" emacs-version)
 
 (require 'undercover)
-;; (undercover "org-sync-gtasks.el"
-;;             (:report-format 'lcov)
-;;             (:send-report nil)
-;;             (:exclude "org-sync-gtasks-api.el"))
-(setq undercover-force-coverage t)
-(undercover "org-sync-gtasks.el" (:report-format 'simplecov)
-            (:send-report nil))
+(undercover "org-sync-gtasks.el"
+            (:report-format 'lcov)
+            (:send-report nil)
+            (:exclude "org-sync-gtasks-api.el"))
+;; (setq undercover-force-coverage t)
+;; (undercover "org-sync-gtasks.el" (:report-format 'simplecov)
+;;             (:send-report nil))
 
 (require 'org-sync-gtasks)
 ;;; test-helper.el ends here
